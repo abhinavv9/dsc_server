2510e864bba3df613dfa451cc9aba89977a05bc5
Yash <khareyash05@gmail.com>
2022-02-20 17:29:07 +0530

    added support for cors

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors);
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 var check;
 
