6c193c45b62d0ff579ea5b236103aecac8153ec5
abhinavv9 <68147871+abhinavv9@users.noreply.github.com>
2022-04-08 17:59:11 +0530

    procfile updated

--- server.js
+++ server.js
@@ -13,10 +13,12 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
   dotenv.config({ path: "secrets.env" });
 }
 app.use(function (req, res, next) {
+  //Enabling CORS
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization"
   );
   next();
 });
